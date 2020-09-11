af74dabac7d5de83f8f6a75380d8abda831a60ad
Lushan <mihodihasan@gmail.com>
2020-09-11 15:56:37 +0600

    content design completed

--- app/src/main/java/com/mihodihasan/marvelsuperheroes/main/adapter/ContentAdapter.kt
+++ app/src/main/java/com/mihodihasan/marvelsuperheroes/main/adapter/ContentAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.mihodihasan.marvelsuperheroes.R
 import com.mihodihasan.marvelsuperheroes.main.model.Comics
 import com.mihodihasan.marvelsuperheroes.main.model.ComicsResult
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_view_main_list.view.*
 
 class ContentAdapter(val context: Context, private val list: MutableList<ComicsResult>) : RecyclerView.Adapter<ContentAdapter.ContentVH>() {
@@ -18,7 +19,10 @@ class ContentAdapter(val context: Context, private val list: MutableList<ComicsR
         R.layout.item_view_main_list, parent, false))
 
     override fun onBindViewHolder(holder: ContentVH, position: Int) {
-        holder.itemView.textView.text = list[position].description
+        holder.itemView.content_description.text = list[position].description
+        holder.itemView.comic_title_tv.text = list[position].title
+        holder.itemView.date_modified_tv.text = list[position].modified
+        Picasso.get().load(list[position].thumbnail.path.plus(".").plus(list[position].thumbnail.extension).replace("http://","https://")).into(holder.itemView.content_img_view)
     }
 
     override fun getItemCount(): Int = list.size
