b0b5c3f2d0e7f2a3e5eed57c1b545e9d2cb1f659
Lushan <mihodihasan@gmail.com>
2020-09-12 18:31:37 +0600

    date formatted

--- app/src/main/java/com/mihodihasan/marvelsuperheroes/main/adapter/ContentAdapter.kt
+++ app/src/main/java/com/mihodihasan/marvelsuperheroes/main/adapter/ContentAdapter.kt
@@ -8,8 +8,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.mihodihasan.marvelsuperheroes.R
 import com.mihodihasan.marvelsuperheroes.main.model.Comics
 import com.mihodihasan.marvelsuperheroes.main.model.ComicsResult
+import com.mihodihasan.marvelsuperheroes.utils.Constants
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_view_main_list.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ContentAdapter(val context: Context, private val list: MutableList<ComicsResult>) : RecyclerView.Adapter<ContentAdapter.ContentVH>() {
 
@@ -19,11 +22,18 @@ class ContentAdapter(val context: Context, private val list: MutableList<ComicsR
         R.layout.item_view_main_list, parent, false))
 
     override fun onBindViewHolder(holder: ContentVH, position: Int) {
-        holder.itemView.content_description.text = list[position].description
+        holder.itemView.content_description.text = if (list[position].description == null) "No Details Available" else list[position].description
         holder.itemView.comic_title_tv.text = list[position].title
-        holder.itemView.date_modified_tv.text = list[position].modified
+        holder.itemView.date_modified_tv.text = if (beautifyDate(list[position].modified)==null) list[position].modified else beautifyDate(list[position].modified)
         Picasso.get().load(list[position].thumbnail.path.plus(".").plus(list[position].thumbnail.extension).replace("http://","https://")).into(holder.itemView.content_img_view)
     }
 
     override fun getItemCount(): Int = list.size
+
+    private fun beautifyDate(serverDate:String):String?{
+        val simpleDateFormatParser = SimpleDateFormat(Constants.SERVER_DATE_FORMAT, Locale.ENGLISH)
+        val simpleDateFormatFormatter = SimpleDateFormat(Constants.BEAUTIFY_DATE_FORMAT, Locale.ENGLISH)
+        val date = simpleDateFormatParser.parse(serverDate)
+        return if (date == null) null else simpleDateFormatFormatter.format(date)
+    }
 }
--- app/src/main/java/com/mihodihasan/marvelsuperheroes/utils/Constants.kt
+++ app/src/main/java/com/mihodihasan/marvelsuperheroes/utils/Constants.kt
@@ -9,5 +9,7 @@ class Constants {
         const val CONTENT_API_MAX_PAGE = "CONTENT_API_MAX_PAGE"
         const val CONTENT_API_LAST_HIT = "CONTENT_API_LAST_HIT"
         const val DEFAULT_DATE_FORMAT = "EEE MMM dd HH:mm:s z yyyy"
+        const val SERVER_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
+        const val BEAUTIFY_DATE_FORMAT = "MMM dd, yyyy"
     }
 }
